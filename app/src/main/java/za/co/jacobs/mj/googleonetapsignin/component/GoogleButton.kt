1b0b34ff697a58af60e894c9bf59e8c5237ce06f
MJJacobs01 <marthinusjacobs@gmail.com>
2023-11-11 14:34:45 +0200

    Google button
    Took 28 minutes

---
+++ app/src/main/java/za/co/jacobs/mj/googleonetapsignin/component/GoogleButton.kt
@@ -0,0 +1,91 @@
+package za.co.jacobs.mj.googleonetapsignin.component
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.*
+import za.co.jacobs.mj.googleonetapsignin.R
+import za.co.jacobs.mj.googleonetapsignin.ui.theme.*
+
+/**
+ * Created by MJ Jacobs on 2023/11/11 at 14:07
+ */
+
+@Composable
+fun GoogleButton(
+    modifier: Modifier = Modifier,
+    loadingState: Boolean = false,
+    primaryText: String = "Sign in with Google",
+    secondaryText: String = "Please wait ...",
+    icon: Int = R.drawable.google_g_logo,
+    shape: Shape = MaterialTheme.shapes.medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    progressIndicatorColor: Color = LoadingBlue,
+    onClick: () -> Unit
+) {
+    var buttonText by remember { mutableStateOf(primaryText) }
+
+    LaunchedEffect(key1 = loadingState) {
+        buttonText = if (loadingState) secondaryText else primaryText
+    }
+
+    Surface(
+        modifier = modifier.clickable(enabled = !loadingState) { onClick() },
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = "Google G Logo",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = buttonText)
+            if (loadingState) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier.size(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewGoogleButton() {
+    GoogleButton(
+        loadingState = true,
+        onClick = {
+
+        }
+    )
+}
--- app/src/main/java/za/co/jacobs/mj/googleonetapsignin/ui/theme/Color.kt
+++ app/src/main/java/za/co/jacobs/mj/googleonetapsignin/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ErrorRed = Color(0xFFFF6c60)
+val InfoGreen = Color(0xFF00c096)
+val LoadingBlue = Color(0xFF1a73e8)
