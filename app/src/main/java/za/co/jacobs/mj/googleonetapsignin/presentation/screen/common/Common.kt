343d45c248e094a9c3f584734ef14c0dc15b1ca9
MJJacobs01 <marthinusjacobs@gmail.com>
2023-11-19 15:52:13 +0200

    Added StartActivityForResult
    Took 13 minutes

--- app/src/main/java/za/co/jacobs/mj/googleonetapsignin/presentation/screen/common/Common.kt
+++ app/src/main/java/za/co/jacobs/mj/googleonetapsignin/presentation/screen/common/Common.kt
@@ -2,14 +2,72 @@ package za.co.jacobs.mj.googleonetapsignin.presentation.screen.common
 
 import android.app.Activity
 import android.util.*
+import androidx.activity.compose.*
 import androidx.activity.result.*
+import androidx.activity.result.contract.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.*
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import za.co.jacobs.mj.googleonetapsignin.util.*
 
 /**
  * Created by MJ Jacobs on 2023/11/19 at 15:22
  */
+
+@Composable
+fun StartActivityForResult(
+    key: Any,
+    onResultReceived: (String) -> Unit,
+    onDialogDismissed: () -> Unit,
+    launcher: (ManagedActivityResultLauncher<IntentSenderRequest, ActivityResult>) -> Unit
+) {
+    val activity = LocalContext.current as Activity
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult(),
+        onResult = { activityResult ->
+            try {
+                if (activityResult.resultCode == Activity.RESULT_OK) {
+                    val oneTapClient = Identity.getSignInClient(activity)
+                    val credentials = oneTapClient.getSignInCredentialFromIntent(activityResult.data)
+                    val tokenId: String? = credentials.googleIdToken
+                    if (tokenId != null) {
+                        onResultReceived(tokenId)
+                    } else {
+                        Log.d("StartActivityForResult", "Black scrim clicked, dialog clicked")
+                        onDialogDismissed()
+                    }
+                }
+            } catch (e: ApiException) {
+                when (e.statusCode) {
+                    CommonStatusCodes.CANCELED -> {
+                        Log.d("StartActivityForResult", "OneTap dialog cancelled")
+                        onDialogDismissed()
+                    }
+
+                    CommonStatusCodes.NETWORK_ERROR -> {
+                        Log.d("StartActivityForResult", "OneTap Network error")
+                        onDialogDismissed()
+                    }
+
+                    else -> {
+                        Log.d("StartActivityForResult", "${e.message}")
+                        onDialogDismissed()
+                    }
+                }
+            }
+        }
+    )
+
+    LaunchedEffect(
+        key1 = key,
+        block = {
+            launcher(activityLauncher)
+        }
+    )
+}
 
 fun signIn(
     activity: Activity,
