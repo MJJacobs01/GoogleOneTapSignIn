df871b8b4dbf0284b76a5120339eb98a0afb5f51
MJJacobs01 <marthinusjacobs@gmail.com>
2023-12-04 18:50:05 +0200

    Created ProfileTopBar.kt
    Took 16 minutes

---
+++ app/src/main/java/za/co/jacobs/mj/googleonetapsignin/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,15 @@
+package za.co.jacobs.mj.googleonetapsignin.presentation.profile
+
+import androidx.compose.runtime.*
+import androidx.navigation.*
+
+/**
+ * Created by MJ Jacobs on 2023/12/04 at 18:34
+ */
+
+@Composable
+fun ProfileScreen(
+    navController:NavHostController
+) {
+
+}
